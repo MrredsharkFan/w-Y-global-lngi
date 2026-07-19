0799f461e50eac8741cbafcab34de824da5996b3
MrRedSharkFan <haychiu2021@gmail.com>
2026-07-19 18:41:47 +0800

    big one

--- search.js
+++ search.js
@@ -202,8 +202,8 @@ document.querySelectorAll("[data-year]").forEach(btn => {
 
 
 
-function search_time() {
-    var t = document.getElementById("search_input").value
+function search_time(x = document.getElementById("search_input").value) {
+    var t = x
     var r = ""
     if (t[0] != 1) {
         r = "Please insert valid ordinal starting with 1!"
@@ -211,24 +211,44 @@ function search_time() {
         return
     }
     else {
-        
         var l = t.split(",")
-        var guess = Number(l[1])+1
-        var interval = 0.5
-        while (interval > 1e-10) {
-            var p = num_to_lngi(guess+interval)[0]
-            console.log(p)
-            if (Y_Sequence.cmp(p, t) == -1) {
-                guess += interval
-            } else if (p == t) {
-                guess += interval
+        var seq = `1,${Number(l[1]) + 1}`
+        var r = Number(l[1])+1
+        var i = 1
+
+        if (l.length == 1) {
+            document.getElementById("search_result").innerHTML = `This lngi starts at 1,1 :3`
+            return 0
+        }
+
+        if (l.length == 2) {
+            document.getElementById("search_result").innerHTML = `Achieved on ${new Date(get_time_inv(r) + st).toLocaleString()}`
+            return r
+        }
+        
+        var check = 2
+        while (i > 1e-10) {
+            //step one: expand
+            seq = Y_Sequence.fs(seq, 2).split(",") //the result is a string, need to convert into list for .at
+            //step two: cut the term to match last term
+            //like when insert 1,3,3; should check 1,4 first and expand into 1,3,10
+            //1,3,10 is the same length as the 3rd term, just compare it
+            //if not, then we don't
+            seq = seq.slice(0, check + 1)
+            //now we do the thing
+            console.log(Number(l[check]),Number(seq.at(-1)))
+            var d = Number(seq.at(-1)) -Number(l[check])
+            i =  i / (2 ** (d + 1)); r += i
+
+            //ready for next iteration
+            seq[check] = Number(l[check]) + 1
+            seq = seq.join(",")
+            check++
+            if (l.length == check) {
                 break
             }
-            interval /= 2
         }
-        console.log(get_time_inv(guess) + st)
-        r = `(If standard) Achieved in ${new Date(get_time_inv(guess)+st).toLocaleString()}`
-        document.getElementById("search_result").innerHTML = r
-        return
+        document.getElementById("search_result").innerHTML = `Achieved on ${new Date(get_time_inv(r)+st).toLocaleString()}`
+        return r
     }
 }
