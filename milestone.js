4349e9ba958f456da342affcb9a6e01aa68ad91c
MrRedSharkFan <haychiu2021@gmail.com>
2026-07-21 00:16:47 +0800

    what?

--- milestone.js
+++ milestone.js
@@ -4,31 +4,32 @@
 //{} -> sub
 
 var milestones = [
-    ["1,2", "w", "First Limit Ordinal, and the start of the journey."],
-    ["1,2,2", "w[2]", "Growth rate of Conway's Chained Array Notation."],
-    ["1,2,3", "w[w]", "The growth rate of Linear Array Notations."],
-    ["1,2,4", "ep{0}", "SCO (Small Cantor Ordinal), also the Proof-Theortic Ordinal (PTO) of Peano Arithematic, and the limit of PrSS"],
-    ["1,2,4,6", "z{0}", "CO (Cantor Ordinal)"],
-    ["1,2,4,6,7", "phi(w,0)", "HCO (Hyper Cantor Ordinal), also the limit of LPrSS"],
-    ["1,2,4,6,8", "phi(1,0,0) / G0", "FSO, also the limit of Transfinite LPrSS"],
-    ["1,2,4,7", "psi(W{2})", "BHO (Bachmann-Howard Ordinal), and it's time for a collapse in Ordinal Markup!"],
-    ["1,2,4,7,9,12", "psi(W{2}*psi{1}(W{2}))", "Endgame of Ordinal Markup, if you guys are wondering..."],
+    ["1,2", "w / (0)(1)", "First Limit Ordinal, and the start of the journey."],
+    ["1,2,2", "w[2] / (0)(1)(1)", "Growth rate of Conway's Chained Array Notation."],
+    ["1,2,3", "w[w] / (0)(1)(2)", "The growth rate of Linear Array Notations."],
+    ["1,2,4", "ep{0} / (0)(1,1)", "SCO (Small Cantor Ordinal), also the Proof-Theortic Ordinal (PTO) of Peano Arithematic, and the limit of PrSS"],
+    ["1,2,4,6", "z{0} / (0)(1,1)(2,1)", "CO (Cantor Ordinal)"],
+    ["1,2,4,6,7", "phi(w,0) / (0)(1,1)(2,1)(3)", "HCO (Hyper Cantor Ordinal), also the limit of LPrSS"],
+    ["1,2,4,6,8", "phi(1,0,0) / G{0} / (0)(1,1)(2,1)(3,1)", "FSO, also the limit of Transfinite LPrSS"],
+    ["1,2,4,7", "psi(W{2}) / (0)(1,1)(2,2)", "BHO (Bachmann-Howard Ordinal), and it's time for a collapse in Ordinal Markup!"],
+    ["1,2,4,7,9,12", "psi(W{2}psi{1}(W{2})) / (0)(1,1)(2,2)(3,1)(4,2)", "Endgame of Ordinal Markup, if you guys are wondering..."],
     //p(W_2*p1(W_2))
-    ["1,2,4,7,11", "psi(W{3})", "Fun fact, I just returned to HK when this ordinal was reached, and I was on the plane!"],
-    ["1,2,4,7,11,16", "psi(W{4})", "Hmph why is this taking so long"],
-    ["1,2,4,8", "psi(W{w})", "BO (???? Ordinal), the start of the 3-row BMS series!"],
-    ["1,2,4,8,10", "psi(W{w}*W)", "This is the point where BMS upgrades. But w-Y, or anything above 1-Y, doesn't yet!"],
-    ["1,2,4,8,11", "psi(W{w}*W{2})", "This is the TRUE upgrading point of Y sequence."],
-    ["1,2,4,8,11,8", "psi(W{w}[2])", "Here's the result of the upgrade!"],
-    ["1,2,4,8,12", "psi(W{w[2]})", "This is somewhat similar to z0, in terms of structure. Not sure how though..."],
-    ["1,2,4,8,12,14", "psi(W{W})", "Bird's Ordinal (BIO), BAN's limit before extensions."],
-    ["1,2,4,8,12,15,9", "psi(I)", "Extended Buchholz Ordinal (EBO), we have to use a new type of uncountables, inaccessibles beyond here."],
-    ["1,2,4,8,12,15,19", "psi(W{I+1})", "Jager's Ordinal (JO)"],
-    ["1,2,4,8,12,16", "psi(I{w})", "Small Inaccessible Ordinal (SIO)"],
-    ["1,2,4,8,12,16,16", "psi(M{w})", "Small Mahlo Ordinal (SMO)"],
-    ["1,2,4,8,12,16,20", "psi(K{w})", "Small Weakly Compact Ordinal (SKO)"],
-    ["1,2,4,8,13", "psi(Pi{w})", "Small Stergert Ordinal (SSO), also the name of my Scratch username!"],
+    ["1,2,4,7,11", "psi(W{3}) / (0)(1,1)(2,2)(3,3)", "Fun fact, I just returned to HK when this ordinal was reached, and I was on the plane!"],
+    ["1,2,4,7,11,16", "psi(W{4}) / (0)(1,1)(2,2)(3,3)(4,4)", "Hmph why is this taking so long"],
+    ["1,2,4,8", "psi(W{w}) / (0)(1,1,1)", "BO (???? Ordinal), the start of the 3-row BMS series!"],
+    ["1,2,4,8,10", "psi(W{w}*W) / (0)(1,1,1)(2,1)", "This is the point where BMS upgrades. But w-Y, or anything above 1-Y, doesn't yet!"],
+    ["1,2,4,8,11", "psi(W{w}*W{2}) / (0)(1,1,1)(2,1)(1,1)(2,2,1)(3,2)", "This is the TRUE upgrading point of Y sequence."],
+    ["1,2,4,8,11,8", "psi(W{w}[2]) / (0)(1,1,1)(2,1)(1,1,1)", "Here's the result of the upgrade!"],
+    ["1,2,4,8,12", "psi(W{w[2]}) / (0)(1,1,1)(2,1,1)", "This is somewhat similar to z0, in terms of structure. Not sure how though..."],
+    ["1,2,4,8,12,14", "psi(W{W}) / (0)(1,1,1)(2,1,1)(3,1)", "Bird's Ordinal (BIO), BAN's limit before extensions."],
+    ["1,2,4,8,12,15,9", "psi(I) / (0)(1,1,1)(2,1,1)(3,1)(2)", "Extended Buchholz Ordinal (EBO), we have to use a new type of uncountables, inaccessibles beyond here."],
+    ["1,2,4,8,12,15,19", "psi(W{I+1}) / (0)(1,1,1)(2,1,1)(3,1)(4,2)", "Jager's Ordinal (JO)"],
+    ["1,2,4,8,12,16", "psi(I{w}) / (0)(1,1,1)(2,1,1)(3,1,1)", "Small Inaccessible Ordinal (SIO)"],
+    ["1,2,4,8,12,16,16", "psi(M{w}) / (0)(1,1,1)(2,1,1)(3,1,1)(3,1,1)", "Small Mahlo Ordinal (SMO)"],
+    ["1,2,4,8,12,16,20", "psi(K{w}) / (0)(1,1,1)(2,1,1)(3,1,1)(4,1,1)", "Small Weakly Compact Ordinal (SKO)"],
+    ["1,2,4,8,13", "psi(Pi{w}) / (0)(1,1,1)(2,2)", "Small Stergert Ordinal (SSO), also the name of my Scratch username!"],
     ["1,2,4,8,14", "(0)(1,1,1)(2,2,1)", "Uh oh I forgor"],
+    ["1,2,4,8,14,15", "(0)(1,1,1)(2,2,1)(3)", "Small Dropping Ordinal (SDO), similar to &phi;(&omega;,0) in structure, and is the limit of SAN, TON (projceted) and several more!"],
     ["1,2,4,8,15", "(0)(1,1,1)(2,2,2)", "Large Rathjen Ordinal (???) (LRO)"],
     ["1,2,4,8,16", "(0)(1,1,1,1)", "Trio Sequence System Ordinal (TSSO), and the start of 4 row! It's only going to be quicker."],
     ["1,2,4,8,16,32", "(0)(1,1,1,1,1)", "Quad Sequence System Ordinal (QSSO)"],
@@ -77,13 +78,18 @@ function update_milestones(page) {
     for (var i = 0; i < 5; i++) {
         var M = page * 5 + i
         if (M < L) {
-            console.log(Date.now() - get_time_inv(milestone_time[M]) - st)
+            var T = Date.now() - get_time_inv(milestone_time[M]) - st
             P[i].style.visibility = "visible"
             P[i].style.backgroundColor = (ct >= milestone_time[M])?"#99ff99":"#ff9999"
-            P[i].innerHTML = `<span style="font-size: 150%">${milestones[M][0]}</span> 
-                            ${(ct >= milestone_time[M]) ? "" : `<br>${formatSeconds((get_time_inv(milestone_time[M]) - Date.now() + st)/1000)  } left`}<br>
-                            ${milestones[M][1]}<hr>
-                            <i>${milestones[M][2]}</i>`
+            P[i].innerHTML = `
+            <div style="display: flex; flex-direction: row">
+                <div style="flex: 1; font-size: 150%">${milestones[M][0]}</div>
+                <div style="flex: 1; text-align: right">
+                    <span style="font-size: 120%">${milestones[M][1]}</span>
+                    ${T<0?`<br>in ${formatSeconds(-T/1000)}`:``}
+                </div>
+            </div><hr>
+            ${milestones[M][2]}`
         } else {
             P[i].style.visibility = "hidden"
             P[i].innerHTML = ""
