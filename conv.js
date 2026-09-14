d1f9c45c5d40c0394af9e743fd4bdaf6f99353ba
MrRedSharkFan <haychiu2021@gmail.com>
2026-09-14 19:35:46 +0800

    fixed a bug relating compress BMS

--- conv.js
+++ conv.js
@@ -8751,7 +8751,7 @@ function convert_From_wY(ord, mode) {
     if (mode == "BMS") {
         if (Y_Sequence.cmp(ord, '1,2,4,8,16,32,64,128,256,512') == -1) {
             if (compress_BMS.checked)
-                return Conv_Y_sequence_BMS(ord).map(p => `(${p.join(',').replace(/(,?0)*$/, '')})`).join('')
+               return Conv_Y_sequence_BMS(ord).map(p => `(${p.join(',')})`).join('').replaceAll(",0", "").replaceAll("(0)","()")
             else
                 return Conv_Y_sequence_BMS(ord).map(p => `(${p.join(',')})`).join('');
         }
@@ -8763,7 +8763,7 @@ function convert_From_wY(ord, mode) {
         if (Y_Sequence.cmp(ord, '1,3') == -1) {
             if (typeof ord == 'string') ord = ord.split(',')
             if (compress_BMS.checked)
-                return Y_to_DBMS(ord).map(p => `(${p.join(',').replace(/(,?0)*$/, '')})`).join('')
+               return Y_to_DBMS(ord).map(p => `(${p.join(',')})`).join('').replaceAll(",0", "").replaceAll("(0)", "()")
             else
                 return Y_to_DBMS(ord).map(p => `(${p.join(',')})`).join('');
         }
